cf7d64c08f72e36d365c0d4a42b7bed2fc5403cf
Jake Roberts <jakeroberts@gmail.com>
2008-05-14 18:23:00 +0000

    Working on moving stuff around

---
+++ demos/applicationdemo/index.php
@@ -0,0 +1,78 @@
+<?php
+	session_start();
+	$_SESSION["file_info"] = array();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+<title>SWFUpload Demos - Application Demo</title>
+<link href="../css/default.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="../swfupload/swfupload.js"></script>
+<script type="text/javascript" src="js/handlers.js"></script>
+<script type="text/javascript">
+		var swfu;
+		window.onload = function () {
+			swfu = new SWFUpload({
+				// Backend Settings
+				upload_url: "../applicationdemo/upload.php",	// Relative to the SWF file
+				post_params: {"PHPSESSID": "<?php echo session_id(); ?>"},
+
+				// File Upload Settings
+				file_size_limit : "2048",	// 2MB
+				file_types : "*.jpg",
+				file_types_description : "JPG Images",
+				file_upload_limit : "0",
+
+				// Event Handler Settings - these functions as defined in Handlers.js
+				//  The handlers are not part of SWFUpload but are part of my website and control how
+				//  my website reacts to the SWFUpload events.
+				file_queue_error_handler : fileQueueError,
+				file_dialog_complete_handler : fileDialogComplete,
+				upload_progress_handler : uploadProgress,
+				upload_error_handler : uploadError,
+				upload_success_handler : uploadSuccess,
+				upload_complete_handler : uploadComplete,
+
+				// Flash Settings
+				flash_url : "../swfupload/swfupload_f9.swf",	// Relative to this file
+
+				custom_settings : {
+					upload_target : "divFileProgressContainer"
+				},
+				
+				// Debug Settings
+				debug: false
+			});
+		};
+	</script>
+</head>
+<body>
+<div id="header">
+	<h1 id="logo"><a href="../">SWFUpload</a></h1>
+	<div id="version">v2.1.0 Beta</div>
+</div>
+<div id="content">
+	<h2>Application Demo</h2>
+	<p>This demo shows how SWFUpload can behave like an AJAX application.  Images are uploaded by SWFUpload then some JavaScript is used to display the thumbnails without reloading the page.</p>
+	<?php
+	if( !function_exists("imagecopyresampled") ){
+		?>
+	<div class="message">
+		<h4><strong>Error:</strong> </h4>
+		<p>Application Demo requires GD Library to be installed on your system.</p>
+		<p>Usually you only have to uncomment <code>;extension=php_gd2.dll</code> by removing the semicolon <code>extension=php_gd2.dll</code> and making sure your extension_dir is pointing in the right place. <code>extension_dir = "c:\php\extensions"</code> in your php.ini file. For further reading please consult the <a href="http://ca3.php.net/manual/en/image.setup.php">PHP manual</a></p>
+	</div>
+	<?php
+	} else {
+	?>
+	<form>
+		<button id="btnBrowse" type="button" style="padding: 5px;" onclick="swfu.selectFiles(); this.blur();"><img src="images/page_white_add.png" style="padding-right: 3px; vertical-align: bottom;">Select Images <span style="font-size: 7pt;">(2 MB Max)</span></button>
+	</form>
+	<?php
+	}
+	?>
+	<div id="divFileProgressContainer" style="height: 75px;"></div>
+	<div id="thumbnails"></div>
+</div>
+</body>
+</html>
---
+++ demos/featuresdemo/upload.php
@@ -0,0 +1,63 @@
+<?php
+	session_start();
+	
+	$upload_good = false;
+	if (!isset($_FILES["Filedata"])) {
+		$upload_good = "Not recieved, probably exceeded POST_MAX_SIZE";
+	}
+	else if (!is_uploaded_file($_FILES["Filedata"]["tmp_name"])) {
+		$upload_good = "Upload is not a file. PHP didn't like it.";
+	} 
+	else if ($_FILES["Filedata"]["error"] != 0) {
+		$upload_good = "Upload error no. " + $_FILES["Filedata"]["error"];
+	} else {
+		$upload_good = "The upload was good";
+	}
+?>
+<p>Upload Page</p>
+
+<p>Here is the list of cookies that the agent sent:</p>
+<ul>
+	<?php
+		foreach ($_COOKIE as $name => $value) {
+			echo "<li>";
+			echo htmlspecialchars($name) . "=" . htmlspecialchars($value);
+			echo "</li>\n";
+		}
+	?>
+</ul>
+<p>Here is the list of query values:</p>
+<ul>
+	<?php
+		foreach ($_GET as $name => $value) {
+			echo "<li>";
+			echo htmlspecialchars($name) . "=" . htmlspecialchars($value);
+			echo "</li>\n";
+		}
+	?>
+</ul>
+<p>Here is the list of post values:</p>
+<ul>
+	<?php
+		foreach ($_POST as $name => $value) {
+			echo "<li>";
+			echo htmlspecialchars($name) . "=" . htmlspecialchars($value);
+			echo "</li>\n";
+		}
+	?>
+</ul>
+<p>Here is the list of the files uploaded:</p>
+<ul>
+	<?php
+		foreach ($_FILES as $name => $value) {
+			echo "<li>";
+			echo htmlspecialchars($name) . "=" . htmlspecialchars($value["name"]);
+			echo "</li>\n";
+		}
+	?>
+</ul>
+<p>Filedata upload status: <?php echo $upload_good; ?>.</p>
+
+<p>Here is the current session id:</p>
+<p><?php echo htmlspecialchars(session_id()); ?></p>
+<p>Compare this to the session id displayed near the top of the Features Demo page. The Flash Player plug-in does not send the correct cookies in some browsers.</p>
